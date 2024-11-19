5f728bb86c839461a85d46dc0fbc3366c7ebd32b
Guilherme Gaigher Netto <ggaighernt@gmail.com>
2024-11-19 16:29:41 -0300

    refactor: rename ConversationCreate to Messages and update schema structure

--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,16 @@
 from pydantic import BaseModel, Field
 
 
-class ConversationCreate(BaseModel):
+class Messages(BaseModel):
+    message: list = Field(..., title="Message", description="Messages")
+
+
+class Conversation(Messages):
     user_id: str = Field(
         ..., title="User ID", description="The user ID of the conversation"
     )
-    message: list[str] = Field(
-        ..., title="Message", description="The messages of the conversation"
-    )
 
 
-class ConversationResponse(ConversationCreate):
+class ConversationResponse(Conversation):
     # Include ID in response schema
     id: str = Field(..., title="ID", description="The ID of the conversation")

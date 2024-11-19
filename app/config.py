9f741c88eabe5936b50405d4a8f34f203ce82813
Guilherme Gaigher Netto <ggaighernt@gmail.com>
2024-11-19 11:25:09 -0300

    feat: add Docker support and configure API service with MongoDB

--- app/config.py
+++ app/config.py
@@ -3,12 +3,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # The base URL of the API
-    HOST: str = "localhost"
+    HOST: str = "mongo"
     PORT: int = 27017
     DB_NAME: str = "mydatabase"
     COLLECTION_NAME: str = "mycollection"
-    USERNAME: str
-    PASSWORD: str
+    USERNAME: str = "root"
+    PASSWORD: str = "example"
 
     class Config:
         env_file = ".env"
